b7088afe8be5afabc48aa8e09acdf796d45f3376
FynnWhatever <fynn.whatever@gmail.com>
2017-01-31 12:26:50 +0100

    Added ID to DatagramPacket

---
+++ app/src/main/java/flo/gameserver/client/UserInput.java
@@ -0,0 +1,12 @@
+package flo.gameserver.client;
+
+public class UserInput {
+	String id;
+	int x;
+	int y;
+	public UserInput(String id, int xvalue, int yvalue){
+		this.id = id;
+		x = xvalue;
+		y= yvalue;
+	}
+}
